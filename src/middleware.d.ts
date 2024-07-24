2baebbecf6358e9ae3e6b4b1ba8ef3f3b9465c91
chuppyy <takhanhthient3@GMAIL.COM>
2024-07-24 22:51:02 +0700

    ll3

--- src/middleware.d.ts
+++ src/middleware.d.ts
@@ -5,12 +5,17 @@ import geoip from 'geoip-lite';
 export default async function middleware(req: NextRequest) {
   //const country = req.geo?.country || req.headers.get('x-vercel-ip-country') || 'VI'
   const ip = req.headers.get('x-forwarded-for') || '';
-  const country = geoip.lookup(ip);
-  console.log("country", country)
+  const geo = geoip.lookup(ip);
+
+ // if (geo && geo.country === 'VN') 
+    
+    return NextResponse.redirect(new URL(`${geo.country}`, req.url))
+   
+  
   // if (['VI', 'VN', 'vi', 'vn'].includes(country ?? '')) {
   //   return NextResponse.redirect(new URL(`${country}`, req.url))
   // }
-  return NextResponse.redirect(new URL(`${country}`, req.url))
+  //return NextResponse.redirect(new URL(`${country}`, req.url))
 }
 
 export const config = {
