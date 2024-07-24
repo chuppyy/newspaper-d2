8b2bb105022d59db94e6d7adba6bf8fc6d8b3657
chuppyy <takhanhthient3@GMAIL.COM>
2024-07-24 22:38:57 +0700

    ll2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,11 @@
 import { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
+import geoip from 'geoip-lite';
 
 export default async function middleware(req: NextRequest) {
-  const country = req.geo?.country || req.headers.get('x-vercel-ip-country') || 'VI'
+  //const country = req.geo?.country || req.headers.get('x-vercel-ip-country') || 'VI'
+  const ip = req.headers.get('x-forwarded-for');
+  const country = geoip.lookup(ip);
   console.log("country", country)
   if (['VI', 'VN', 'vi', 'vn'].includes(country)) {
     return NextResponse.redirect(new URL(`${country}`, req.url))
