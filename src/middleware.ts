d962329891a8a6cd13e2e20b6816d37c1da3c474
chuppyy <takhanhthient3@GMAIL.COM>
2024-07-25 20:32:47 +0700

    k

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,14 +7,13 @@ export default async function middleware(req: any) {
   const country = req.geo?.country || req.headers.get('x-vercel-ip-country')
   const forwarded = req.headers.get('x-forwarded-for');
   const detectedIp = requestIp.getClientIp(req)
-  //const ip = req.headers['x-forwarded-for'] ;
-  //const geo = geoip.lookup(ip);
-//const x=ip+"k"+geo.co;
- // if (geo && geo.country === 'VN') 
-    
+  if (['VN'].includes(country)) {
+    return NextResponse.redirect(new URL('/not-found', req.url))
+  }
+  return NextResponse.next();
 
 
-    return NextResponse.redirect(new URL(`${country}`, req.url))
+    //return NextResponse.redirect(new URL(`${country}`, req.url))
    
   
   // if (['VI', 'VN', 'vi', 'vn'].includes(country ?? '')) {
